370d66408ea687bd90f38b02949ca12334f1a94d
lijiawei <>
2020-06-27 16:45:21 +0800

    Update test_events.py

--- tests/test_events.py
+++ tests/test_events.py
@@ -5,13 +5,17 @@
 # @File    : test_events.py
 # @Software: PyCharm
 
+
 from garbevents.events import GetData
 
 from garbevents.settings import Settings as ST
+import sys
+import os
 
-
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
 'mitmdump -p 8889 -s test_events.py'
-
 
 ST.url = 'https://www.baidu.com/'
 ST.report_path = 'report'
