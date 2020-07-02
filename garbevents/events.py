a8ef465afd4440ba8a75e0f1258699e4966ea266
Pactortester <1456470136@qq.com>
2020-07-02 09:03:28 +0800

    Update events.py

--- garbevents/events.py
+++ garbevents/events.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import os
 import urllib
 import zlib
 import urllib.parse
@@ -54,7 +54,7 @@ class GetData:
             except KeyError:
                 print("暂无事件!")
             event_list = list(set(self.events_list))
-
+            os.mkdir(ST.report_path)
             file = open('{}/now_event.txt'.format(ST.report_path), 'w')
             for line in event_list:
                 file.write(line + '\n')
