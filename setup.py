78721a800cfe889e46e9353c931c1857f85c87cb
Pactortester <1456470136@qq.com>
2020-09-18 18:44:18 +0800

    release v1.1.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name="garbevents",
-    version="1.1.3",
+    version="1.1.4",
     keywords=["pip", "garbevents", "get_events", "buried points"],
     description="grabbing buried points",
     long_description=long_description,
