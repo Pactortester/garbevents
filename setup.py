6a7beff6dcbd82a3b50271e40e66357d8cd1d85b
QingTian203 <65541087+QingTian203@users.noreply.github.com>
2021-02-28 00:02:18 +0800

    feat:新增(神策)细化对事件的值，和统计分类 (#11)
    * feat:新增(神策)细化对事件的值，和统计分类
    
    1.custom_sensors_events.py
    2.新增log日志
    
    * Code formatting
    
    Co-authored-by: QingTian203 <>
    Co-authored-by: Pactortester <1456470136@qq.com>

--- setup.py
+++ setup.py
@@ -40,5 +40,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     platforms="any",
-    install_requires=["mitmproxy"]
+    install_requires=["mitmproxy", "jsonpath"]
 )
--- tests/test_custom_sensors_events.py
+++ tests/test_custom_sensors_events.py
@@ -2,16 +2,17 @@
 
 from garbevents.custom_sensors_events import GetData
 
-
 from garbevents.settings import Settings as ST
 
-'mitmdump -p 8889 -s test_custom_sensors_events.py'
 
-# https://sensors.wenwo.com 爱问医生
-# http://sensorsdata-2.talbrain.com:8106 学而思网校
+'mitmdump -p 8889 -s test_custom_sensors_events.py'
 ST.url = 'https://sensors.wenwo.com'
 ST.report_path = 'report'
-ST.all_events = ['LX_CONVERSATION_MSG_ITEM_FORWAR_CLICK', 'LX_CONVERSATION_SETTING_TOPS_CLICK']
+ST.all_events = ['$WebClick', '$pageview']
+ST.events_properties = {
+    '$WebClick': ['app_name', 'is_login', 'part_name', 'platform_type', 'referrer_path'],
+    '$pageview': ['app_name', 'is_login', 'part_name', 'platform_type', 'referrer_path']
+}
 
 addons = [
     GetData()
