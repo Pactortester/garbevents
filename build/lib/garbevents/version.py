be5fbafbc2299bd60ba51d4b1ee6368aa71abf92
Pactortester <1456470136@qq.com>
2021-06-28 23:36:40 +0800

    fix:修正依赖的第三方库版本

--- build/lib/garbevents/version.py
+++ build/lib/garbevents/version.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "2.0.3"
+__version__ = "2.0.4"
 
 import os
 import sys
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name="garbevents",
-    version="2.0.3",
+    version="2.0.4",
     keywords=["pip", "garbevents", "get_events", "buried points"],
     description="grabbing buried points",
     long_description=long_description,
@@ -40,5 +40,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     platforms="any",
-    install_requires=["mitmproxy", "jsonpath", "paramiko", "emjio"]
+    install_requires=["mitmproxy", "jsonpath", "paramiko", "emoji"]
 )
